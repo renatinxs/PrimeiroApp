14e3e9a64217d0e5103f9056bc76ab7947b782e6
Renato <renato27oliveiracordeiro@gmail.com>
2026-03-29 12:21:49 -0300

    Alterando banco de dados

--- PWIII-DatabaseConnect/PrimeiroBD.sql
+++ PWIII-DatabaseConnect/PrimeiroBD.sql
@@ -14,3 +14,14 @@ insert into usuario(nomeUsu, Cargo, DataNasc)
 			 ('Bruno','Colaborador','2000/10/12');
              
 select * from usuario;
+
+Create table endereco(
+Id int primary key auto_increment,
+CEP varchar(100) not null,
+Estado varchar(70) not null,
+Cidade varchar(70) not null,
+Bairro varchar(70) not null,
+Logradouro varchar(150) not null,
+Complemento varchar(150) not null,
+Numero varchar(15) not null
+);      
